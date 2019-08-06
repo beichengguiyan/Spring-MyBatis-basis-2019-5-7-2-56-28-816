978782ca1a48f8a02c2d32240622b098412a20db
helloworldfish <763104106@qq.com>
2019-08-06 15:24:05 +0800

    add mapper

--- src/main/java/com/example/demo/dao/UserMapper.java
+++ src/main/java/com/example/demo/dao/UserMapper.java
@@ -10,21 +10,24 @@ import java.util.List;
 @Repository
 public interface UserMapper {
     // 1. 实现查询所有用户
-    @Select("select * from user where 1=1")
-    default List<User> getAllUser() {
-        return null;
-    }
+    @Select("select * from user")
+    List<User> findAllPage();
+
     // 2. 根据用户id查询用户
-    @Select("select * from user where id = ${id}")
-    User getUserById(@Param("id") int id);
+    @Select("select * from user where id = #{id}")
+    User findByID(Integer id);
+
     // 3. 添加新用户
-    @Insert("insert into user (id, name) values (${id}, #{name})")
-    int insertNewUser(User u);
+    @Insert("insert into user(id,name) values(${id},#{name})")
+    @Options(useGeneratedKeys = true,keyProperty = "id")
+    Integer addUser(User user);
+
     // 4. 根据用户id删除用户
     @Delete("delete from user where id = ${id}")
-    int deleteUserById(@Param("id") int id);
+    Integer deleteUser(Integer id);
+
     // 5. 根据用户id更新用户
-    @Update("update user set id = #{id},name = #{name} where id = ${id_1}")
-    int updateUserById(User u ,@Param("id_1") int id);
+    @Update("update user set name = #{user.name} where id = ${id1}")
+    Integer updateUser(@Param("user") User user,@Param("id1") Integer id1);
 
 }
